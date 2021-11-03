6d05385a77fdc7b42f733454be35b05e4621f122
Giller <69960363+Gila-Johanna-Hofmann@users.noreply.github.com>
2021-11-03 08:31:38 +0100

    more trying to convert to hex

--- scripts.js
+++ scripts.js
@@ -4,15 +4,40 @@ var sliderGreen = document.getElementById("myRangeGreen");
 var output = document.getElementById("showhex");
 var changecolor = document.getElementsByTagName("body");
 
-// Display the default slider value
-output.innerHTML = sliderRed.value; 
+
+
+//get slider values
+var red = sliderRed.value;
+var blue = sliderBlue.value*-1; //the blue range ouputs negative values --> enables "inverting" the range in order to get a brighter colors overall
+var green = sliderGreen.value;
+
+//calculate hex values from slider values
+var intToHex = function (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+var r = intToHex(red);
+var b = intToHex(blue);
+var g = intToHex(green);
+
+//calculate rgb-hex-code from r, g, b
+var fullColorHex = function(r,g,b) {   
+  return '#'+r+g+b;
+};
 
 // Update the current slider value (each time you drag the slider handle)
-sliderRed.oninput = function() {
-  output.innerHTML = this.value;
+const inputhandlder = function() {
+  output.innerHTML = fullColorHex(r, g, b);
 }
 
-function calculateColor() {
-    
-}
+sliderRed.addEventListener(input, inputhandlder)
+
+// Display the default slider value
+//output.innerHTML = fullColorHex(r, g, b); 
+
+
 
