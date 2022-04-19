36a53a9fc3c10a5d63de3b98fe178f9d76a3f69c
Gila.Hofmann <gila.hofmann@holistic-net.de>
2022-04-19 15:25:50 +0200

    refactored most functions

--- scripts.js
+++ scripts.js
@@ -1,44 +1,69 @@
 const sliderRed = document.getElementById("myRangeRed");
 const sliderBlue = document.getElementById("myRangeBlue");
 const sliderGreen = document.getElementById("myRangeGreen");
+
+//establish default background-color
 let red = (parseInt(sliderRed.value) * -1).toString();
 let green = sliderGreen.value;
 let blue = (parseInt(sliderBlue.value) * -1).toString();
 let output = document.getElementById("show-hex");
 
-sliderRed.onchange = function () {
-  red = (parseInt(sliderRed.value) * -1).toString();
-  document.body.style.backgroundColor = ["rgb(",red,",",green,",",blue,")"].join("");
-};
-
-sliderBlue.onchange = function () {
-  blue = (parseInt(sliderBlue.value) * -1).toString();
-  document.body.style.backgroundColor = ["rgb(",red,",",green,",",blue,")"].join("");
-};
-
-sliderGreen.onchange = function () {
-  green = sliderGreen.value;
-  document.body.style.backgroundColor = ["rgb(",red,",",green,",",blue,")"].join("");
-};
-
+//calculate hex-value from rgb-values
 function RGBToHex(r,g,b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-  
-    if (r.length == 1)
-      r = "0" + r;
-    if (g.length == 1)
-      g = "0" + g;
-    if (b.length == 1)
-      b = "0" + b;
-  
-    return "#" + r + g + b;
-  }
+  r = r.toString(16);
+  g = g.toString(16);
+  b = b.toString(16);
 
-window.onchange = function () {
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+//parses rgb-string-values to int-values and calculates hex-value
+function changeHex () {
   var redout = parseInt(red);
   var greenout = parseInt(green);
-  var blueout = parseInt(blue);
+  var blueout = parseInt(blue); 
   output.innerHTML =  RGBToHex(redout, greenout, blueout);
+}
+
+//changes document body background color based on slider-input
+function changeBackground() {
+  document.body.style.backgroundColor = ["rgb(",red,",",green,",",blue,")"].join("");
+  changeHex();
+}
+
+//returns slider-value in reverse order
+function getColorReverse(slider) {
+  return (parseInt(slider.value) * -1).toString();
+}
+
+//returns slider-value
+function getColor(slider) {
+  return slider.value;
+}
+
+sliderRed.addEventListener('change', (event) => 
+{ red = getColorReverse(sliderRed);
+  changeBackground();}
+);
+
+sliderBlue.addEventListener('change', (event) => 
+{ blue = getColorReverse(sliderBlue);
+  changeBackground();}
+);
+
+sliderGreen.addEventListener('change', (event) => 
+{ red = getColor(sliderGreen);
+  changeBackground();}
+);
+
+//display default hex-value
+window.onload = function () {
+  changeHex();
 };
